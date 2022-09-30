31b6ae6c6e16a130757360f3006675e1a26633b3
Thariq <alg70597@gmail.com>
2022-09-30 20:57:03 +0700

    T1-PROFILE_2019110119

---
+++ resources/views/portofolio.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>portofolio</title>
+    </head>
+
+    <body>
+        <h1>My Portofolio</h1>
+        <img src="img\bayer.jpg" width="200" height="100">
+        <img src="img\400006.jpg" width="200" height="100">
+    </body>
+    </html>
